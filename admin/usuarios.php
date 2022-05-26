9ba9f378e19eaea7ae23182492e80de19e3b629d
Thaís Sechetin <93165270+thaissechetin@users.noreply.github.com>
2022-05-26 16:01:32 -0300

    add mais um usuario

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,21 +1,25 @@
-<?php 
-require "../inc/cabecalho-admin.php"; 
+<?php
+require "../inc/funcoes-usuarios.php";
+require "../inc/cabecalho-admin.php";
 
+$usuarios = lerUsuarios($conexao);
+$quantidade = count($usuarios);
 
 ?>
+
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Usuários <span class="badge badge-primary">X</span>
+			Usuários <span class="badge badge-info"><?=$quantidade?></span>
 		</h2>
 
 		<p class="lead text-right">
-				<a class="btn btn-primary" href="usuario-insere.php">Inserir novo usuário</a>
+			<a class="btn btn-info" href="usuario-insere.php">Inserir novo usuário</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="thead-light">
 					<tr>
@@ -27,30 +31,31 @@ require "../inc/cabecalho-admin.php";
 				</thead>
 
 				<tbody>
+					<?php foreach ($usuarios as $usuario) { ?>
 
-					<tr>
-						<td> Nome... </td>
-						<td> E-mail... </td>
-						<td> Tipo... </td>
-						<td class="text-center">
-							<a class="btn btn-warning btn-sm" 
-							href="usuario-atualiza.php">
-								Atualizar
-							</a>
-						</td>
-						<td class="text-center">
-							<a class="btn btn-danger btn-sm excluir" 
-							href="usuario-exclui.php">
-								Excluir
-							</a>
-						</td>
-					</tr>
 
-				</tbody>                
+
+						<tr>
+							<td> <?=$usuario['nome']?> </td>
+							<td> <?=$usuario['email']?> </td>
+							<td> <?=$usuario['tipo']?> </td>
+							<td class="text-center">
+								<a class="btn btn-warning btn-sm" href="usuario-atualiza.php?id=<?=$usuario['id']?>">
+									Atualizar
+								</a>
+							</td>
+							<td class="text-center">
+								<a class="btn btn-danger btn-sm excluir" href="usuario-exclui.php?id=<?=$usuario['id']?>">
+									Excluir
+								</a>
+							</td>
+						</tr>
+					<?php } ?>
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
-<?php require "../inc/rodape-admin.php"; ?> 
+<?php require "../inc/rodape-admin.php"; ?>
